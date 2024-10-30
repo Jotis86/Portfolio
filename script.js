f1a78d0893b8a8a0899933010f8d486650b4efb5
Jotis86 <juaneloyduranbon@hotmail.com>
2024-10-30 13:30:31 +0100

    up

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
+        habilidades[0].classList.add("python");
+        habilidades[1].classList.add("sql");
+        habilidades[2].classList.add("powerbi");
+        habilidades[3].classList.add("gitgithub");
+        habilidades[4].classList.add("machinelearning");
         habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[6].classList.add("criticalthinking");
+        habilidades[7].classList.add("teamwork");
+        habilidades[8].classList.add("problemsolving");
+        habilidades[9].classList.add("adaptability");
     }
 }
 
